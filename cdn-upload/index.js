f4ad4e370b1fa58ee04ca23c1502f78e3ca81cc2
AdamMomen <adammuman81@gmail.com>
2020-04-13 19:55:52 +0100

    CDN Selector

--- cdn-upload/index.js
+++ cdn-upload/index.js
@@ -1,6 +1,11 @@
 const CloudinaryUploader = require('./cloudinary-uploader');
 const GoogleUploader = require("./google-uploader")
 
+/**
+ * CDN selector
+ * where it selects the uploading service provider
+ */
+
 module.exports = function createCdnUploader(config, type) {
     switch (type) {
         case 'google':
