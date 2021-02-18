435e46df171a85f641c6033dfee1e0aeb8026376
Adam <adammuman81@gmail.com>
2021-02-18 19:34:25 +0100

    removed extra packages

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
 const config = require("./config/config"),
-  redis = require("./db/index"),
-  createCdnUploader = require('./cdn-upload/index.js.js'),
+  createCdnUploader = require("./cdn-upload/index.js"),
   bodyParser = require("body-parser"),
   express = require("express"),
   cors = require("cors");
-
 
 /**
  * Express App
@@ -15,31 +13,34 @@ let app = express();
 /**
  * Middleware for disapling x - powered - by message
  */
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 
 /**
- * Cross Origin Sources middleware 
+ * Cross Origin Sources middleware
  */
 app.use(cors());
 
 /**
  * Bodyparser middleware image buffer size to 50mb limit
  */
-app.use(bodyParser.json({
-  limit: '50mb'
-}));
+app.use(
+  bodyParser.json({
+    limit: "50mb",
+  })
+);
 
 /**
  * Bodyparser middleware urlencoded not extended
  */
-app.use(bodyParser.urlencoded({
-  extended: false,
-}));
-
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 /**
  * @GET
- * static Api endpoint 
+ * static Api endpoint
  */
 app.get("/", (req, res) => {
   res.send("Screenify");
@@ -47,67 +48,62 @@ app.get("/", (req, res) => {
 
 /**
  * @POST
- * /upload Api endpoint 
+ * /upload Api endpoint
  */
 app.use("/api/upload", async (req, res, next) => {
-  const {
-    buffer
-  } = req.body;
+  const { buffer } = req.body;
   try {
-    const imageUrl = await upload(buffer, "google")
-    res
-      .status(200)
-      .json({
-        success: true,
-        url: imageUrl
-      })
+    const imageUrl = await upload(buffer, "google");
+    res.status(200).json({
+      success: true,
+      url: imageUrl,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
 /**
  * Uploading function returns the url of the uploaded picture.
- * @param {blob} serializeBlob 
- * @param {string} cdnType 
+ * @param {blob} serializeBlob
+ * @param {string} cdnType
  */
 function upload(serializeBlob, cdnType) {
   return new Promise((resolve, reject) => {
-    const bytes = new Uint8Array(serializeBlob.split(','));
-    const uploader = createCdnUploader(config, cdnType)
+    const bytes = new Uint8Array(serializeBlob.split(","));
+    const uploader = createCdnUploader(config, cdnType);
     uploader
       .upload(Buffer.from(bytes))
-      .then(url => {
+      .then((url) => {
         redis.create({
           url: url,
           cdnType: cdnType,
-          createdAt: new Date()
-        })
-        resolve(url)
+          createdAt: new Date(),
+        });
+        resolve(url);
       })
-      .catch(e => {
+      .catch((e) => {
         try {
-          upload(buffer, cdnType == "google" ? "cloudinary" : "google")
+          upload(buffer, cdnType == "google" ? "cloudinary" : "google");
         } catch (err) {
-          reject(e)
+          reject(e);
         }
       });
-  })
+  });
 }
 
 /**
- * Error Handler middleware 
+ * Error Handler middleware
  */
 app.use(function (err, req, res, next) {
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message);
 });
 
-
 /**
  * Server Starter 🚀
  */
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
   console.log("🚀 Connected to port", server.address().port);
