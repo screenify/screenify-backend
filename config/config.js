99e67c5c0e9a8542de4f7544b3b2cbea1efebb63
Adam <adammuman81@gmail.com>
2021-05-31 17:48:51 +0200

    refactor the codebase to typescript

--- config/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-/**
- * Enfironment Varibales Config Interface
- */
-module.exports = config = {
-  googleProjectId: process.env.GOOGLE_PROJECT_ID,
-  googleBucketName: process.env.GOOGLE_BUCKET_NAME,
-};
